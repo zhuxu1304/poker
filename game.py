c5851d2da64e26e00c628a80fe4e15dea101ddba
zhuxu1304 <zhuxu1304@gmail.com>
2022-03-28 19:19:06 +0200

    create structure for game

--- game.py
+++ game.py
@@ -21,11 +21,46 @@ class Game(Rules):
             button = self.players[self.button]
             small_blind = self.button + 1
             big_blind = self.button + 2
+
+            # small and big blind pay in the pot
+            self.players[small_blind].change_money(-50)
+            self.pot += 50
+            self.players[big_blind].change_money(-100)
+            self.pot += 100
+
+            # deliever cards
             self.cards, self.community_cards = self.get_random_deck(self.cards, 5)
 
             # give each player two cards
-            for each in self.players:
-                self.cards, cards = self.get_random_deck(self.cards, 5)
-                each.set_card(cards)
+            for player in self.players:
+                self.cards, cards = self.get_random_deck(self.cards, 2)
+                player.set_card(cards)
                 print(each.cards)
 
+            # first round ask for action
+            for player in self.players:
+                player.ask_for_action_firstround()
+
+            # reveal 3 cards
+
+            # second round ask for action
+            for player in self.players:
+                player.ask_for_action()
+
+            # reveal 1 card
+
+            # third round ask for action
+            for player in self.players:
+                player.ask_for_action()
+
+            # reveal 1 card
+
+            # fourth round ask for action
+            for player in self.players:
+                player.ask_for_action()
+
+            # announce winner, give him money in pot
+
+            # reset pot, choose new button
+            self.pot = 0
+            self.button += 1
